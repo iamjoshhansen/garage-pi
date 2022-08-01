03fffbb8e484e49b7e8018da448c7a6f96666249
Joshua Hansen <hansenlj@us.ibm.com>
2022-07-31 20:47:47 -0600

    logs and all high

--- test/register.js
+++ test/register.js
@@ -1,8 +1,8 @@
 const Gpio = require('onoff').Gpio;
 
-const pinA = new Gpio(4, 'out');
+const pinA = new Gpio(4, 'high');
 const pinB = new Gpio(17, 'high');
-const pinC = new Gpio(18, 'low');
+const pinC = new Gpio(18, 'high');
 
 process.on('SIGINT', () => {
   pinA.unexport();
@@ -12,15 +12,18 @@ process.on('SIGINT', () => {
 
 async function run() {
 
+  console.log(`Initial wait...`);
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
   while (true) {
+    console.log(`All on`);
     pinA.write(true);
     pinB.write(true);
     pinC.write(true);
 
     await new Promise((resolve) => setTimeout(resolve, 500));
 
+    console.log(`All off`);
     pinA.write(false);
     pinB.write(false);
     pinC.write(false);
